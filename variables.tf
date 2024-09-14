edfe5487668771ab5bf1101385ae232931e1e054
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-09-14 18:35:51 -0300

    default variables

--- variables.tf
+++ variables.tf
@@ -64,83 +64,92 @@ variable "service_hosts" {
 }
 
 variable "scale_type" {
-
+  default = null
 }
 
 variable "task_minimum" {
-
+  default = 3
 }
 
 variable "task_maximum" {
-
+  default = 10
 }
 
 #Autoscaling CPU
 
 variable "scale_out_cpu_threshold" {
-
+  default = 80
 }
 
 variable "scale_out_adjustment" {
-
+  default = 1
 }
 
 variable "scale_out_comparison_operator" {
-
+  default = "GreaterThanOrEqualToThreshold"
 }
 
 variable "scale_out_statistic" {
-
+  default = "Average"
 }
 
 variable "scale_out_period" {
-
+  default = 60
 }
 
 variable "scale_out_evaluation_periods" {
-
+  default = 2
 }
 
 variable "scale_out_coodown" {
-
+  default = 60
 }
 
 variable "scale_in_cpu_threshold" {
-
+  default = 30
 }
 
 variable "scale_in_adjustment" {
-
+  default = -1
 }
 
 variable "scale_in_comparison_operator" {
+  default = "LessThanOrEqualToThreshold"
 
 }
 
 variable "scale_in_statistic" {
+  default = "Average"
 
 }
 
 variable "scale_in_period" {
-
+  default = 120
 }
 
 variable "scale_in_evaluation_periods" {
+  default = 3
 
 }
 
 variable "scale_in_coodown" {
+  default = 120
+
 
 }
 
 variable "scale_tracking_cpu" {
+  default = 80
 
 }
 
 variable "alb_arn" {
+  default = null
 
 }
 
 variable "scale_tracking_requests" {
+  default = 0
+
 
 }
