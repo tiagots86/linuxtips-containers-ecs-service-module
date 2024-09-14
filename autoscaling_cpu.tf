ba4801e1d23d4bb17dadb1be91e90200ab494ea5
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-09-14 10:42:08 -0300

    autoscaling cpu cloudwatch alarm

--- autoscaling_cpu.tf
+++ autoscaling_cpu.tf
@@ -20,3 +20,28 @@ resource "aws_appautoscaling_policy" "cpu_high" {
     }
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  count = var.scale_type == "cpu" ? 1 : 0
+
+  alarm_name = format("%s-%s-cpu-scale-out", var.cluster_name, var.service_name)
+
+  comparison_operator = var.scale_out_comparison_operator
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  statistic           = var.scale_out_statistic
+
+  period             = var.scale_out_period
+  evaluation_periods = var.scale_out_evaluation_periods
+  threshold          = var.scale_out_cpu_threshold
+
+  dimensions = {
+    ClusterName = var.cluster_name
+    ServiceName = var.service_name
+  }
+
+  alarm_actions = [
+    aws_appautoscaling_policy.cpu_high[count.index].arn
+  ]
+
+}
