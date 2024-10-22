b69eb01113232511317c65cec472f13ff72ae8ad
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-10-22 07:36:42 -0300

    correção service discovery

--- service_discovery.tf
+++ service_discovery.tf
@@ -14,7 +14,7 @@ resource "aws_service_discovery_service" "main" {
     routing_policy = "MULTIVALUE"
   }
 
-  health_check_config {
+  health_check_custom_config {
     failure_threshold = 1
   }
 
