2719c55d6d81f65f08204420299dc500404a5dfa
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-11-20 18:24:13 -0300

    deploy codedeploy

--- deploy_codedeploy.tf
+++ deploy_codedeploy.tf
@@ -9,3 +9,27 @@ resource "local_file" "appspec" {
     CAPACITY_PROVIDERS  = var.service_launch_type
   })
 }
+
+resource "null_resource" "deploy_codedeploy" {
+  count = var.deployment_controller == "CODE_DEPLOY" ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "aws deploy create-deployment --cli-input-yaml file://${path.module}/${aws_codedeploy_app.main[count.index].name}.yaml"
+    environment = {
+      AWS_REGION = var.region
+    }
+  }
+
+  triggers = {
+    task_definition = aws_ecs_task_definition.main.revision
+  }
+
+  depends_on = [
+    aws_ecs_service.main, 
+    aws_ecs_task_definition.main, 
+    aws_codedeploy_app.main, 
+    aws_codedeploy_deployment_group.main, 
+    local_file.appspec 
+  ]
+
+}
